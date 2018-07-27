2a649c0512ba043b420f4317f13e12f04e9ed4e2
Andiakatz4 <andiakatz@gmail.com>
2018-07-27 14:51:10 -0700

    added jquery for functionality

--- public/assets/js/burgerJQuery.js
+++ public/assets/js/burgerJQuery.js
@@ -1,12 +1,32 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function () {
+        $(".change-devoured").on("click", function(event) {
+          var id = $(this).data("id");
+          var newDevoured = $(this).data("devoured");
+      
+          var newDevouredState = {
+            devoured: newDevoured
+          };
+      
+    $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newDevouredState
+      }).then(
+        function() {
+          console.log("changed sleep to", newDevouredState);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+
+
     $(".create-form").on("submit", function (event) {
         // Make sure to preventDefault on a submit event.
         event.preventDefault();
-
         var newBurger = {
-            name: $("#ca").val().trim(),
-            devoured: $("[name=devoured]:checked").val().trim()
+            burger_name: $("#ca").val().trim(),
+            devoured: 0,
         };
 
         // Send the POST request.
